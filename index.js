72d9b31ee20e9e1e262be4581912838a449521f0
Arkham <arkham195@gmail.com>
2020-01-25 01:38:25 +0100

    test
    test

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const Eris = require("eris");
 
 var bot = new Eris("token");
 bot.on("ready", () => {
-    console.log("Ready!");
+	console.log("Ready!");
 });
 bot.on("messageCreate", (msg) => {
-    if(msg.content === "!ping") {
-        bot.createMessage(msg.channel.id, "Pong!");
-    }
+	if (msg.content === "!ping") {
+		bot.createMessage(msg.channel.id, "Pong!");
+	}
 });
 bot.connect();
