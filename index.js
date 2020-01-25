e51bfa087c903ed4757120aa009b25a38fa3340e
ArkadRadz <arkadiusz.radziewicz@protonmail.com>
2020-01-25 02:16:03 +0100

    hide token.txt and use it for connection

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const Eris = require("eris");
+const fs = require('fs');
 
-var bot = new Eris("token");
+var token = '';
+
+fs.readFile('./token.txt', 'utf-8', (err, data) => {
+  if(err) { throw err; }
+  token = data;
+});
+
+var bot = new Eris(token);
+
 bot.on("ready", () => {
     console.log("Ready!");
 });
